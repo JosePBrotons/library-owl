1e8ad608d815f38f60be690c59c2a30b8b36ac83
Jose P. Brotons R <jose.brotons@almundo.com>
2020-11-28 22:16:39 -0500

    Updated tests.

--- src/components/settings/index.test.tsx
+++ src/components/settings/index.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Settings from '.';
 
-test('renders login component correctly', () => {
+test('renders settings component correctly', () => {
     const settingsCmp = renderer.create(<Settings />).toJSON();
     expect(settingsCmp).toMatchSnapshot();
 });
